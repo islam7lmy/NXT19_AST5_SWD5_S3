01bdbb7a668f9c8c50636431e049a2c0a89d4283
islam helmy <islam7lmy@gmail.com>
2026-09-04 21:37:54 +0300

    0. string vs stringbuilder 1. string formatting [format , interpolition] 2. conditional statment [if , switch] 3. loop statment [for , while]

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo01
+﻿using System.Text;
+
+namespace Demo01
 {
     /// <summary>
     /// 
@@ -255,7 +257,410 @@
             #endregion
 
             #region string vs stringbuilder
+            //string x = "hello"; //syntactic sugar for string x = new string("hello");
+            //Console.WriteLine(x.GetHashCode());
+            //x = "world"; //x = new string("world"); 
+            ////x is now referring to a new string object in memory,
+            ////the old string "hello" is now eligible for garbage collection
+            ////string is immutable => once created, it cannot be changed
+            //Console.WriteLine(x.GetHashCode());
+            //x += "!!!"; //x = x + "!!!"; //x = new string("world!!!");
+            //Console.WriteLine(x.GetHashCode());
+            ///////////////////////////////////////////////////////////////////////////
+            //StringBuilder Y = new StringBuilder("hello");
+            ////Console.WriteLine(Y.GetHashCode());
+            //Y.Append(" world"); //modifies the existing StringBuilder object, rather than creating a new one
+            ////Console.WriteLine(Y.GetHashCode());
+            //Y.Clear(); //clears the content of the StringBuilder object, but does not change its reference
+            //Y.AppendLine("new content"); //appends a new line to the StringBuilder object
+            //Y.Insert(0, "hello world"); //inserts "hello world" at the beginning of the StringBuilder object
+            //Y.Remove(0, "hello world".Length); //removes "hello world" from the beginning of the StringBuilder object
+            //Y.Replace("new content", "replaced content"); //replaces "new content" with "replaced content" in the StringBuilder object
+            //Console.WriteLine(Y);
+            #endregion
 
+            #region string formating
+            // Equition : 4 + 2 = 6
+            //int X = 4, Y = 2;
+            //string result = "Equition : " + X + " + " + Y + " = " + (X + Y); //string concatenation
+
+            //string result1 = "Equition : ";
+            //result1 += X;
+            //result1 += " + ";
+            //result1 += Y;
+            //result1 += " = ";
+            //result1 += (X + Y);
+
+            //StringBuilder result2 = new StringBuilder("Equition : ");
+            //result2.Append(X);
+            //result2.Append(" + ");
+            //result2.Append(Y);
+            //result2.Append(" = ");
+            //result2.Append(X + Y);
+
+            //string result3 = string.Format("Equition : {0} + {1} = {2}", X, Y, X + Y); //string formatting => Placeholder based formatting 
+
+            //string result4 = $"Equition : {X} + {Y} = {X + Y}"; // string interpolation => Expression based formatting
+
+            //string result5 = $"Equition : {X} + {Y} = {X + Y}" + $" the result is true";
+
+            //result5 = new string($"Equition : {X} + {Y} = {X + Y}");
+            //result5 += $" the result is true";
+
+            //Console.WriteLine(result);
+            //Console.WriteLine(result1);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(result3);
+            //Console.WriteLine(result4);
+            #endregion
+
+            #region Conditional statment
+            //if (condition) //true or false
+            //{
+            //    //statements to execute if condition is true
+            //}
+            //else if(condition2)
+            //{
+            //    //statements to execute if condition2 is true
+            //}
+            //else
+            //{
+            //    //statements to execute if all conditions are false
+            //}
+
+
+            ///jump table
+            //varible = value;
+            //switch (varible)
+            //{ 
+            //    case value1:
+            //        //statements to execute if varible == value1
+            //        break;
+            //    case value2:
+            //        //statements to execute if varible == value2
+            //        break;
+            //    case value3:
+            //        //statements to execute if varible == value3
+            //        break;
+            //    default:
+            //        //statements to execute if varible does not match any case
+            //        break;
+            //}
+
+            #region Ex01 :write a program to take number in first quarter in year and print the month name 
+            #region If statment
+            ///write a program to take number in first quarter in year and print the month name 
+            /// {1 , 2 , 3} => {january , February , March} 
+            /// protictive code
+            //Console.WriteLine("insert number of month in first qurter of year :");
+            //int month = int.Parse(Console.ReadLine());
+            //if (int.TryParse(Console.ReadLine(), out int month))
+            //{
+            //    if (month == 1)
+            //    {
+            //        Console.WriteLine("January");
+            //    }
+            //    else if (month == 2)
+            //    {
+            //        Console.WriteLine("February");
+            //    }
+            //    else if (month == 3)
+            //    {
+            //        Console.WriteLine("March");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid month number");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //}
+
+            //if (!int.TryParse(Console.ReadLine(), out int month))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+            //if (month == 1)
+            //{
+            //    Console.WriteLine("January");
+            //}
+            //else if (month == 2)
+            //{
+            //    Console.WriteLine("February");
+            //}
+            //else if (month == 3)
+            //{
+            //    Console.WriteLine("March");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid month number");
+            //}
+
+
+            //if (!int.TryParse(Console.ReadLine(), out int month))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+            //Console.WriteLine(
+            //    month == 1 ? "January" :
+            //    month == 2 ? "February" :
+            //    month == 3 ? "March" :
+            //    "Invalid month number"
+            //    ); 
+            #endregion
+
+            #region Switch statment
+            //Console.WriteLine("insert number of month in first qurter of year :");
+            //if (!int.TryParse(Console.ReadLine(), out int month))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+            //switch (month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("January");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("February");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("March");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month number");
+            //        break;
+            //} 
+            #endregion
+            #endregion
+
+            #region Ex02
+            //int salary = 3000;
+            //switch(salary)
+            //{
+            //    case 3000:
+            //        Console.WriteLine("option 03");
+            //        //Console.WriteLine("option 02");
+            //        //Console.WriteLine("option 01");
+            //        //break;
+            //        goto case 2000;
+            //    case 2000:
+            //        Console.WriteLine("option 02");
+            //        //Console.WriteLine("option 01");
+            //        //break;
+            //        goto case 1000;
+            //    case 1000:
+            //        Console.WriteLine("option 01");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Option");
+            //        break;
+            //}
+            #endregion
+
+            #region Calculater
+            //take number then opertation then number and show result
+            //calculater do (+ - * / %)
+            //write program to do that and make it protictive
+            #region not clean code
+            //Console.WriteLine("Enter first number:");
+            //if (int.TryParse(Console.ReadLine(), out int num1))
+            //{
+            //    if (char.TryParse(Console.ReadLine(), out char operation))
+            //    {
+            //        if (int.TryParse(Console.ReadLine(), out int num2))
+            //        {
+            //            int result = 0;
+            //            switch (operation)
+            //            {
+            //                case '+':
+            //                    result = num1 + num2;
+            //                    break;
+            //                case '-':
+            //                    result = num1 - num2;
+            //                    break;
+            //                case '*':
+            //                    result = num1 * num2;
+            //                    break;
+            //                case '/':
+            //                    if (num2 != 0)
+            //                    {
+            //                        result = num1 / num2;
+            //                    }
+            //                    else
+            //                    {
+            //                        Console.WriteLine("Error: Division by zero is not allowed.");
+            //                    }
+            //                    break;
+            //                case '%':
+            //                    if (num2 != 0)
+            //                    {
+            //                        result = num1 % num2;
+            //                    }
+            //                    else
+            //                    {
+            //                        Console.WriteLine("Error: Modules by zero is not allowed.");
+            //                    }
+            //                    break;
+            //                default:
+            //                    Console.WriteLine("Invalid operator. Please enter a valid operator (+, -, *, /, %).");
+            //                    break;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //            return; // Exit the program or handle the error as needed
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid operator.");
+            //        return; // Exit the program or handle the error as needed
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //} 
+            #endregion
+
+            #region clean code
+            //Console.WriteLine("Enter first number:");
+            //if (!int.TryParse(Console.ReadLine(), out int num1))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+            //Console.WriteLine("Enter operation:");
+            //if (!char.TryParse(Console.ReadLine(), out char operation)
+            //    || (operation != '+' && operation != '-' && operation != '*' 
+            //    && operation != '/' && operation != '%'))
+
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid operator.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+            //Console.WriteLine("Enter seconed number:");
+            //if (!int.TryParse(Console.ReadLine(), out int num2))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //    return; // Exit the program or handle the error as needed
+            //}
+
+
+            //int result = 0;
+            //switch (operation)
+            //{
+            //    case '+':
+            //        result = num1 + num2;
+            //        break;
+            //    case '-':
+            //        result = num1 - num2;
+            //        break;
+            //    case '*':
+            //        result = num1 * num2;
+            //        break;
+            //    case '/':
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine("Error: Division by zero is not allowed.");
+            //            return; // Exit the program or handle the error as needed
+            //        }
+            //        result = num1 / num2;
+            //        break;
+            //    case '%':
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine("Error: Modules by zero is not allowed.");
+            //            return; // Exit the program or handle the error as needed
+            //        }
+            //        result = num1 % num2;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid operator. Please enter a valid operator (+, -, *, /, %).");
+            //        break;
+            //}
+            //Console.WriteLine($"{num1} {operation} {num2} = {result}");
+            #endregion
+
+            #endregion
+            #endregion
+
+            #region Loop statments
+            // 10 steps to print 1 to 10
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+            //Console.WriteLine("3");
+            //Console.WriteLine("4");
+            //Console.WriteLine("5");
+            //Console.WriteLine("6");
+            //Console.WriteLine("7");
+            //Console.WriteLine("8");
+            //Console.WriteLine("9");
+            //Console.WriteLine("10");
+
+            #region For
+            // start => i = 1
+            // condition => i <= 10 => true
+            // execute => Console.WriteLine(i);
+            // increment => i++
+            // repeat => condition => i <= 10 => true
+            // repeat => execute => Console.WriteLine(i);
+            // repeat => increment => i++
+
+            //for (int i = 1; i <= 10; i++) // ++i
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int i = 1; //start
+            //for(; ; i++)
+            //{
+            //    if (!(i <= 10)) //condition
+            //        break; //exit loop
+
+            //    if (i % 2 == 0)
+            //    {
+            //        continue; //skip even numbers
+            //        //i++; //increment
+            //    }
+            //    Console.WriteLine(i); //execute
+
+            //    //i++; //increment
+            //}
+            #endregion
+
+            #region While
+            //Console.WriteLine("insert 0 if you want to exist");
+            //string input = Console.ReadLine();
+            //while(input != "0")
+            //{
+            //    Console.WriteLine($"you entered : {input}");
+            //    Console.WriteLine("insert 0 if you want to exist");
+            //    input = Console.ReadLine();
+            //}
+
+
+            //Console.WriteLine("Enter first number:");
+            //while (!int.TryParse(Console.ReadLine(), out int num1))
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //}
+
+            //[quiz] write calculater program using while loop and switch statment and make it protictive
+            #endregion
             #endregion
         }
 
